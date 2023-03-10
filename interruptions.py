ccc679d664ad61789dd2f7a61f858269b1774ec5
christianDUCROS <96232175+christianDUCROS@users.noreply.github.com>
2023-03-10 07:46:26 +0100

    Update interruptions.py

--- interruptions.py
+++ interruptions.py
@@ -3,43 +3,43 @@ import time
 from machine import Pin
 
 bt1_ON_OFF = Pin(5, Pin.IN, Pin.PULL_UP)
-etat_action_bt1 = 0
+etat_commande_bt1 = 0
 
 led = Pin('LED', Pin.OUT) #sortie lampe
 led.off()
 
 bt2_ON_OFF = Pin(6, Pin.IN, Pin.PULL_UP)
-etat_action_bt2 = 0
+etat_commande_bt2 = 0
 
 start = time.ticks_ms()  
 
 def programme_interruption_bt1(pin):
-    global etat_action_bt1
+    global etat_commande_bt1
     global start
     delta = time.ticks_diff(time.ticks_ms(), start) # Antirebond
     if delta > 150 : 
         start = time.ticks_ms()    
-        if etat_action_bt1 :
-            etat_action_bt1 = 0
+        if etat_commande_bt1 :
+            etat_commande_bt1 = 0
             led.off()
             print('OFF-bt1')
         else :
-            etat_action_bt1 = 1
+            etat_commande_bt1 = 1
             led.on()
             print('ON-bt1')
         
 
 def programme_interruption_bt2(pin):
-    global etat_action_bt2
+    global etat_commande_bt2
     global start
     delta = time.ticks_diff(time.ticks_ms(), start) # Antirebond
     if delta > 150 : 
         start = time.ticks_ms()    
-        if etat_action_bt2 :
-            etat_action_bt2 = 0
+        if etat_commande_bt2 :
+            etat_commande_bt2 = 0
             print('OFF-bt2')
         else :
-            etat_action_bt2 = 1
+            etat_commande_bt2 = 1
             print('ON-bt2')
 
 
